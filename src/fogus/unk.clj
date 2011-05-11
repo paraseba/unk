6b72fd149878fe5bb418f32e8d3232e3ef3e3d52
fogus <mefogus@gmail.com>
2011-05-11 19:24:11 -0400

    Added utility functions clear-cache, snapshot, and has-cache? and corresponding tests

--- src/fogus/unk.clj
+++ src/fogus/unk.clj
@@ -1,3 +1,15 @@
+;; unk.clj -- A pluggable, manipulable memoization library for Clojure
+
+;; by Michael Fogus - <http://fogus.me/fun/trammel>
+;; Feb. 2011
+
+; Copyright (c) Michael Fogus, 2011. All rights reserved.  The use
+; and distribution terms for this software are covered by the Eclipse
+; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+; which can be found in the file COPYING the root of this
+; distribution.  By using this software in any fashion, you are
+; agreeing to be bound by the terms of this license.  You must not
+; remove this notice, or any other, from this software.
 (ns fogus.unk
   "unk is a memoization library offering functionality above Clojure's core `memoize`
    function in the following ways:
@@ -28,7 +40,11 @@
    associated with `e`")
   (miss    [cache e ret]
    "Is meant to be called if the cache is determined to **not** contain a
-   value associated with `e`"))
+   value associated with `e`")
+  (clear   [cache]
+   "Is used to signal that the cache should be cleared of all elements.
+   The contract is that said cache should return an empty instance of its
+   own type."))
 
 (deftype BasicCache [cache]
   CacheProtocol
@@ -39,6 +55,8 @@
   (hit [this item] this)
   (miss [_ item result]
     (BasicCache. (assoc cache item result)))
+  (clear [_]
+    (BasicCache. {}))
   Object
   (toString [_] (str cache)))
 
@@ -59,12 +77,24 @@
     (PluggableMemoization. f (miss cache item result)))
   (lookup [_ item]
     (lookup cache item))
+  (clear [_]
+    (PluggableMemoization. f (clear cache)))
   Object
   (toString [_] (str cache)))
 
 ;; # Public API
 
 (defn memo
+  "Used as a more flexible alternative to Clojure's core `memoization`
+   function.  Memoized functions built using `memo` will respond to
+   the core unk manipulable memoization utilities.  As a nice bonus,
+   you can use `memo` in place of `memoize` without any additional
+   changes.
+
+   You can access the memoization cache directly via the `:unk` key
+   on the memoized function's metadata.  However, it is advised to
+   use the unk primitives instead as implementation details may
+   change over time."
   ([f] (memo #(PluggableMemoization. % (basic-cache)) f))
   ([cache-factory f]
      (let [cache (atom (cache-factory f))]
@@ -72,7 +102,33 @@
         (fn [& args] 
           (let [cs (swap! cache through f args)]
             @(lookup cs args)))
-        {:cache cache}))))
+        {:unk cache}))))
+
+(defn snapshot
+  "Returns a snapshot of an unk-placed memoization cache.  By snapshot
+   you can infer that what you get is only the cache contents at a
+   moment in time."
+  [memoized-fn]
+  (when-let [cache (:unk (meta memoized-fn))]
+    (.cache (.cache @cache))))
+
+(defn has-cache?
+  "Returns true if a function has an unk-placed cache, false otherwise."
+  [f]
+  (boolean (:unk (meta f))))
+
+(defn clear-cache!
+  "Reaches into an unk-memoized function and clears the cache.  This is a
+   destructive operation and should be used with care.
+
+   Keep in mind that depending on what other threads or doing, an
+   immediate call to `snapshot` may not yield an empty cache.  That's
+   cool though, we've learned to deal with that stuff in Clojure by
+   now."
+  [f]
+  (when (has-cache? f)
+    (when-let [cache  (:unk (meta f))]
+      (swap! cache (constantly (clear @cache))))))
 
 (comment
   (def cache (basic-cache))
@@ -89,5 +145,5 @@
   ; "Elapsed time: 0.049 msecs"
   ;=> [108 108]
 
-  @(:cache (meta sometimes-slowly))
+  @(:unk (meta sometimes-slowly))
 )
--- test/unk/test/core.clj
+++ test/unk/test/core.clj
@@ -5,3 +5,32 @@
 
 (deftest test-lookup
   (is (= :robot (lookup (miss (BasicCache. {}) '(servo) :robot) '(servo)))))
+
+(def id (memo identity))
+
+(deftest test-cache-innards
+  (let [CACHE_IDENTITY (:unk (meta id))]
+    (testing "That an unk-populated function looks correct at its inception"
+      (is (has-cache? id))
+      (is (snapshot id))
+      (is (empty? (snapshot id))))
+    (testing "That an unk-populated function looks correct after some interactions"
+      ;; Memoize once
+      (is (= 42 (id 42)))
+      ;; Now check to see if it looks right.
+      (is (find (snapshot id) '(42)))
+      (is (= 1 (count (snapshot id))))
+      ;; Memoize again
+      (is (= [] (id [])))
+      (is (find (snapshot id) '([])))
+      (is (= 2 (count (snapshot id))))
+      (testing "that upon memoizing again, the cache should not change"
+        (is (= [] (id [])))
+        (is (find (snapshot id) '([])))
+        (is (= 2 (count (snapshot id)))))
+      (testing "if clearing the cache works as expected"
+        (is (clear-cache! id))
+        (is (empty? (snapshot id)))))
+    (testing "that after all manipulations, the cache maintains its identity"
+      (is (identical? CACHE_IDENTITY (:unk (meta id)))))))
+
