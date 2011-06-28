e5dac8dc59dae1e23453d287205e58f6ceae33d3
fogus <mefogus@gmail.com>
2011-06-28 13:50:30 -0400

    Added lein 0.6.0 and lein-marginalia 0.6.0 to dev deps

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.3.1"]
-                     [marginalia "0.5.1"]])
+                     [marginalia "0.6.0"]
+                     [lein-marginalia "0.6.0"]])
 
