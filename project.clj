f432242d5eb4df54b7abf4d99ee826230af20f0f
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-08-08 17:59:15 -0300

    0.9.2 version depending on clache 0.6.1
    clache 0.6.1 fixes an important bug in TTL cache

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject unk "0.9.1"
+(defproject unk "0.9.2"
   :description "A memoization library for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [clache "0.5.0"]]
+                 [clache "0.6.1"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.3.1"]
